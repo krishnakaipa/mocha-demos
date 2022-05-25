0963b4720944c6d003b9f311a673bd566cb8e8c7
Krishna Kaipa <102549178+krishnakaipa@users.noreply.github.com>
2022-05-25 12:30:26 +0400

    Added test file

--- app/factorial.js
+++ app/factorial.js
@@ -1,3 +1,4 @@
+const test = require("../test.js")
 class Factorial {
     constructor(){
         
---
+++ app/test.js
@@ -0,0 +1,12 @@
+class Test{
+    constructor(){
+        
+    };
+
+    test() {
+        return 1;
+    };
+};
+
+module.exports = Test;
+
