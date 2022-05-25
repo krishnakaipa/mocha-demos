22674d3fd89eae2be0a623c4a226353ab38dfac0
Krishna Kaipa <102549178+krishnakaipa@users.noreply.github.com>
2022-05-25 12:12:04 +0400

    Update factorial.js

--- app/factorial.js
+++ app/factorial.js
@@ -7,12 +7,18 @@ class Factorial {
         if (n < 0) {
             throw new Error(`Negative number!`);
         }
+        if (n == 0) {
+            return 1;
+        }
         return (n !== 1) ? n * this.factorialShort(n - 1) : 1;
     };
 
     factorialSimple(n) {
         if (n < 0) {
             throw new Error(`Negative number!`);
+        }
+        if (n == 0) {
+            return 1;
         }
         if (n !== 1) {
             return n * this.factorialShort(n-1);
